9b2ceb7cd123f4eccb965a9ab92adb7fea72b829
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-01 20:53:15 +0600

    simple node client website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAdduser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+    e.target.reset();
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUsers = [...users, data];
+        console.log(newUsers)
+        setUsers(newUsers);
+      });
+      
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleAdduser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+      <h1>Users: {users.length}</h1>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {" "}
+            Name: {user.name} email: {user.email}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
